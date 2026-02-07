b6a212c69f4b96637b58a23498e872a5d8fb1eb3
AMAN KUMAR KESHRI <amankeshripc@gmail.com>
2026-02-07 12:40:36 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict";// treat all JS code as newer version 
+
+// alert(3+3) we are using node js not browser if i use this in browser then i got alert pop-up
+//https://tc.39es // ECMA script ,decide standard 
+
+let name="aman"
+let age=18
+let isloggedIn =false
+let state=null
+
+//number
+//bigint
+//string =" "
+//boolean
+//null   ->standalone value  means khali hai 
+//undefined
+//symbol => unique ka baat krne k liye
+//object
+
+console.log(typeof("aman"))
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined

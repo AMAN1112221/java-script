3682b30e548e01cd291022b8c6288010c0627d6a
AMAN KUMAR KESHRI <amankeshripc@gmail.com>
2026-02-07 11:52:25 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId =14425
+let accountEmail="aman@gmail.com"
+var accountPassword="12345"
+accountcity="jaipur"
+let accountState;
+
+accountEmail="monu@gmail.com"
+accountPassword="46446"
+
+console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])

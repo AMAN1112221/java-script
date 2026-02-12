2ed04e2ba679fbd0ba463248e93c23408115ecac
AMAN KUMAR KESHRI <amankeshripc@gmail.com>
2026-02-12 04:31:48 +0000

    number and maths

---
+++ .devcontainer/01_basics/08_nums.js
@@ -0,0 +1,20 @@
+const score=400
+console.log(typeof(score));
+
+const balance=new Number(100)
+console.log(balance)// yha alg se Number dikhaye ga 
+
+
+console.log(balance.toString());//now balance is string 
+//console.log(balance.lenght());
+
+
+
+const othernumber=23.89666
+console.log(othernumber.toFixed(2));//it will fix precision ex 23.90
+console.log(othernumber.toPrecision(4))//23.90
+
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'));//10,00,000
+

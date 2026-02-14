f4eb3b738010aa5f4765add8cbbf1aa9538ea5af
AMAN KUMAR KESHRI <amankeshripc@gmail.com>
2026-02-14 05:20:37 +0000

    few propeties of math

---
+++ .devcontainer/01_basics/09_maths.js
@@ -0,0 +1,18 @@
+console.log(Math);
+console.log(Math.abs(-4));//4 and positive no same hi rhe ga 
+console.log(Math.round(4.6));//5
+console.log(Math.ceil(4.2));//5
+console.log(Math.floor(4.9));//4
+console.log(Math.min(4,9,3,5));
+console.log(Math.max(5,6,8,4));
+
+console.log(Math.random());//0to1 any number 
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);//10 to 20 number aaye ga
+
+
